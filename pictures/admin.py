ebeb716c1899fadd60de4027c026854a9cb152dd
David Griff <david@w5h.com>
2020-10-04 14:13:05 -0400

    Pictures list working basic

--- pictures/admin.py
+++ pictures/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Picture
 
+# Tell admin site to show it for editing
 admin.site.register(Picture)
--- pictures/views.py
+++ pictures/views.py
@@ -4,6 +4,15 @@
 
 from django.shortcuts import render
 
+# go get the data model
+from .models import Picture
+
 # Create your views here.
 def home(request):
-    return render(request, 'pictures/home.html') #extra folder so we can see where it is and avoid name clash across apps
+    # Collect data from db via the data model to the page
+    pictures = Picture.objects
+
+    # Now fire up the HTML page with data..
+    return render(request, 'pictures/home.html', {"pictures": pictures} )
+    # (notice the extra 'pictures sub-folder so we can see
+    # where it is and avoid name clash across apps..)

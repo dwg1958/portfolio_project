a79e597b447e58ebb0d9c42b9a80b8120ce45291
David Griff <david@w5h.com>
2020-10-14 00:33:44 -0400

    add picture description to admin panel

--- pictures/models.py
+++ pictures/models.py
@@ -7,3 +7,6 @@ from django.db import models
 class Picture(models.Model):
     image       = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.description

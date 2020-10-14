1fbb4a1b63eabb3224e952ad085310a04273542f
David Griff <david@w5h.com>
2020-10-14 14:08:17 -0400

    changed /staticfiles to /static

--- pictures/models.py
+++ pictures/models.py
@@ -8,5 +8,6 @@ class Picture(models.Model):
     image       = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
 
+    #Change picture title in admin panel
     def __str__(self):
         return self.description
